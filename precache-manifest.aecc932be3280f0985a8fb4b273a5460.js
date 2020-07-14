b64f3bd6536b843e895b6680b83e69edbe671701
Gextox <gabriel_il@live.com>
2020-07-14 02:32:22 -0300

    Updates

---
+++ precache-manifest.aecc932be3280f0985a8fb4b273a5460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ecc58e42c8f7354aecb63b9d4cd854",
+    "url": "/news-explorer/index.html"
+  },
+  {
+    "revision": "ae874496a9317d01d7eb",
+    "url": "/news-explorer/static/css/main.4f174dc5.chunk.css"
+  },
+  {
+    "revision": "80abae14ef5cf1213b29",
+    "url": "/news-explorer/static/js/2.19435e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-explorer/static/js/2.19435e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae874496a9317d01d7eb",
+    "url": "/news-explorer/static/js/main.2e51913d.chunk.js"
+  },
+  {
+    "revision": "0f955ad2a3c27b84f7aa",
+    "url": "/news-explorer/static/js/runtime-main.ff6ac87c.js"
+  }
+]);
